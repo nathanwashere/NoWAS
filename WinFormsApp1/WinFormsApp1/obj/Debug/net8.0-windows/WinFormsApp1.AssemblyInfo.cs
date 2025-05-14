6f7a796f5d65a53553c9a7b63c2f5176eadf9bc5
LizVainer <liza.vainer2018@gmail.com>
2025-05-14 11:45:32 +0300

    Merged into main through liz

--- WinFormsApp1/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92a623c9e1e4bfca7ebf325cdfe544fbb6e6be55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be406d6c576321f21e33bbd53ea1ea8b32a3db4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
