6c63d084dd701251a3ed4f0165cafc1062f77b71
LizVainer <liza.vainer2018@gmail.com>
2025-05-25 19:37:07 +0300

    test

--- WinFormsApp1/inserting_Questions_delete_Questions.Tests/obj/Debug/net8.0-windows/inserting_Questions_delete_Questions.Tests.AssemblyInfo.cs
+++ WinFormsApp1/inserting_Questions_delete_Questions.Tests/obj/Debug/net8.0-windows/inserting_Questions_delete_Questions.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("inserting_Questions_delete_Questions.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e3d022739c49de8d379f179f71ae2fe9e671027")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09b9a81c2aaf4ef85b5682afbda9f303f3dd103e")]
 [assembly: System.Reflection.AssemblyProductAttribute("inserting_Questions_delete_Questions.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("inserting_Questions_delete_Questions.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
