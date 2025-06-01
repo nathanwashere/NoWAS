7b2a2f1cdeb8e1295c928f0d8bc21355e60883c0
zechariah315 <zechariah151299@gmail.com>
2025-06-01 12:32:04 +0300

    Changing images

--- WinFormsApp1/WinFormsApp1/Properties/Resources.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Properties/Resources.Designer.cs
@@ -73,6 +73,26 @@ namespace WinFormsApp1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap A_large_question_mar_png {
+            get {
+                object obj = ResourceManager.GetObject("A+large+question+mar.png", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap A_large_white_questi_png {
+            get {
+                object obj = ResourceManager.GetObject("A+large+white+questi.png", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap background1 {
             get {
                 object obj = ResourceManager.GetObject("background1", resourceCulture);
