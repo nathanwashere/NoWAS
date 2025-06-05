9a5b4066c929e15d0e50641454d88273e62644fc
zechariah315 <zechariah151299@gmail.com>
2025-06-05 13:26:42 +0300

    Inserting questions

--- WinFormsApp1/inserting_Questions_delete_Questions.Tests/obj/Debug/net8.0-windows/inserting_Questions_delete_Questions.Tests.AssemblyInfo.cs
+++ WinFormsApp1/inserting_Questions_delete_Questions.Tests/obj/Debug/net8.0-windows/inserting_Questions_delete_Questions.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("inserting_Questions_delete_Questions.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72a354d8e06aea4145504d81d0238bb49cf296e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e37cb3adf566aedb18c05e0305bd2e25f54ad4")]
 [assembly: System.Reflection.AssemblyProductAttribute("inserting_Questions_delete_Questions.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("inserting_Questions_delete_Questions.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
